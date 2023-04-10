4e45a858ad822bc21a0b4804e28fff620521ce62
Lastlilith <lastlilith.2014@gmail.com>
2023-04-10 23:44:09 +0900

    implemented basic splash and logo

--- app/src/main/java/com/imnidasoftware/daydiary/MainActivity.kt
+++ app/src/main/java/com/imnidasoftware/daydiary/MainActivity.kt
@@ -3,11 +3,13 @@ package com.imnidasoftware.daydiary
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.imnidasoftware.daydiary.ui.theme.DayDiaryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             DayDiaryTheme {
 
