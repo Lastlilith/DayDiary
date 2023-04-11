1ce7c9b1020bd3da31a429350f31cfbf583999c2
Lastlilith <lastlilith.2014@gmail.com>
2023-04-11 13:55:57 +0900

    implemented basic navigation

--- app/src/main/java/com/imnidasoftware/daydiary/MainActivity.kt
+++ app/src/main/java/com/imnidasoftware/daydiary/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
+import com.imnidasoftware.daydiary.navigation.Screen
+import com.imnidasoftware.daydiary.navigation.SetupNavGraph
 import com.imnidasoftware.daydiary.ui.theme.DayDiaryTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +15,11 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             DayDiaryTheme {
-
+                val navController = rememberNavController()
+                SetupNavGraph(
+                    startDestination = Screen.Authentication.route,
+                    navController = navController
+                )
             }
         }
     }
---
+++ app/src/main/java/com/imnidasoftware/daydiary/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.imnidasoftware.daydiary.util
+
+object Constants {
+
+    const val APP_ID = "mydaydiary-rsfmc"
+    const val CLIENT_ID = "561867093073-ilspq10pqkqkvjehr9a8ktk88fng89bj.apps.googleusercontent.com"
+
+    const val WRITE_SCREEN_ARGUMENT_KEY  = "diaryId"
+}
